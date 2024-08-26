fe0dfa8f2fa7b373e8f0124710876df3d7f4ee9f
Thiago Henrique Martinelli <thmartinelli@rd.com.br>
2024-08-26 13:35:51 -0300

    :sparkles: :star: Finished the model and serving api

--- main.py
+++ main.py
@@ -7,15 +7,7 @@ import uvicorn
 
 app = FastAPI()
 
-# Carregar o modelo
 model = tf.saved_model.load('module_with_signature')
-
-
-def load_and_process_image(image_path):
-    image = Image.open(image_path).convert('L')
-    image = image.resize((28, 28))
-    image_array = np.array(image).reshape((1, 28, 28, 1)).astype('float32') / 255
-    return image_array
 
 def predict(model, image_array):
     logits = model(image_array)
@@ -24,20 +16,16 @@ def predict(model, image_array):
 
 @app.get("/")
 async def root():
-    return {"message": "Welcome!"}
+    return {"message": "Welcome! This service will predict an image based on the MNIST dataset"}
 
 @app.post("/predict")
 async def predict_image(file: UploadFile = File(...)):
     """
     Predicts the class of an MNIST image.
     """
-    # Carregar a imagem enviada
     image = Image.open(file.file).convert('L')
-    # Redimensionar a imagem para 28x28 pixels
     image = image.resize((28, 28))
-    # Converter a imagem para um array NumPy
     image_array = np.array(image).reshape((1, 28, 28, 1)).astype('float32') / 255
-    # Fazer a previsão
     prediction = predict(model, image_array)
     return {"predicted_class": str(prediction)}
 
